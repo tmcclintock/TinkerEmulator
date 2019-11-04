fc669a375004caf55e42e593a2f6ac7764b2ee45
Tom McClintock <tmcclintock89@gmail.com>
2019-11-04 11:53:29 +0000

    Working on the emulator in C.

---
+++ src/tinker_emulator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Squared exponential kernel function
+double kernel(double*x1, double*x2, double*lambdas){
+  return 0;
+}
+
+//Emulate the sub-parameters; void for now
+//double* emulate(double*cos, double*subpars){
+void emulate(double*cosmological_parameters, double*subpars){
+  int i, j;
+  return;
+}
+
+//Predict tinker parameters; void for now
+//double* predict_tinker_parameters(double*cos, double redshift, double*tpars){
+void predict_tinker_parameters(double*cosmological_parameters,
+			       double redshift, double*tinker_parameters){
+  int i,j;
+  return;
+}
+
+int main(){
+  printf("test\n");
+  return 0;
+}

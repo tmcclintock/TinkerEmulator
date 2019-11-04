8ce2cf600aba31f55ed85e6ebdbb1f5c248b2fb3
CHUN-HAO TO <chto@stanford.edu>
2019-11-04 04:26:23 -0800

    modify tinker_emulator.c

--- src/tinker_emulator.c
+++ src/tinker_emulator.c
@@ -16,12 +16,36 @@ void emulate(double*cosmological_parameters, double*subpars){
 
 //Predict tinker parameters; void for now
 //double* predict_tinker_parameters(double*cos, double redshift, double*tpars){
-void predict_tinker_parameters(double*cosmological_parameters,
-			       double redshift, double*tinker_parameters){
-  int i,j;
-  return;
-}
+//void predict_tinker_parameters(double*cosmological_parameters,
+//			       double redshift, double*tinker_parameters){
+//  int i,j;
+//  return;
+//}
 
+typedef struct{
+    //parameters of equation 7 in 1907.13167.
+    double A; 
+    double a; 
+    double B; 
+    double b;
+    double C;
+    double c;
+} Emu_tinker_param; 
+
+typedef struct{
+    double ombh2;  
+    double omch2; 
+    double w0; 
+    double n_s;
+    double H0; 
+    double ln_10_10_A_s;  //ln(10^10As)
+    double N_eff;
+} Emu_tinker_cosmo ;
+//Predict tinker parameters;
+Emu_tinker_param predict_tinker_parameters(Emu_tinker_cosmo emutinker, double redshift){
+    Emu_tinker_param test;
+    return test; 
+}
 int main(){
   printf("test\n");
   return 0;
